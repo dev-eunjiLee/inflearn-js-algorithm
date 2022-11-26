0ddf322d547dc97d1da33d5f8db4ecdbcbfc0fd7
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-11-26 22:43:40 +0900

    FIX: (강의 내용) 4. 1부터 N까지의 합

--- src/section_1/4.ts
+++ src/section_1/4.ts
@@ -12,3 +12,14 @@ function solution(n: number): number {
 
 console.log(solution(10));
 console.log(solution(6));
+
+function lectureSolution(n: number): number {
+  let answer = 0;
+  for (let i = 0; i <= n; i++) {
+    answer += i;
+  }
+  return answer;
+}
+
+console.log(lectureSolution(10));
+console.log(lectureSolution(6));
