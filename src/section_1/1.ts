a85e78d4b5cfecb068f7ba16efbdc875e74cdb62
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-11-26 21:56:22 +0900

    FIX: (수업 응용) 1. 세 수 중 최솟값

--- src/section_1/1.ts
+++ src/section_1/1.ts
@@ -2,9 +2,13 @@
  * 100이하의 자연수 A, B, C를 입력받아 세 수 중 가장 작은 값을 출력하는 프로그램을 작성하세요.(정렬을 사용하면 안됩니다)
  */
 
-function solution(a:number, b:number, c:number){
+function getLowerNumber(a: number, b:number):number{
+    return a<b ? a : b
+}
 
-    return Math.min(a,b,c)
+function solution(a:number, b:number, c:number){
+    const answer = getLowerNumber(getLowerNumber(a,b), c)
+    return answer
 }
 
 console.log(solution(2, 5, 1))
