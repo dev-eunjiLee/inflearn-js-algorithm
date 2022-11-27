fef0ab4ea7fc95ac0d94694e39d3c8641581f26a
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-11-27 22:55:37 +0900

    ADD: (먼저 풀어보기) 13. 대소문자 변환

---
+++ src/section_1/13.ts
@@ -0,0 +1,12 @@
+/**
+ * 대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+ */
+function solution(s: string): string {
+  return s.split("").reduce((prev, curr) => {
+    if (curr === curr.toUpperCase()) prev += curr.toLowerCase();
+    else prev += curr.toUpperCase();
+    return prev;
+  }, "");
+}
+
+console.log(solution("StuDY"));
