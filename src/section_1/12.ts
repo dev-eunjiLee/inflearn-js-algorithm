345306683cd804879d0af887525e6d102c3cbbd1
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-11-27 22:51:43 +0900

    FIX: (강의) 12. 대문자로 통일

--- src/section_1/12.ts
+++ src/section_1/12.ts
@@ -7,3 +7,27 @@ function solution(s: string): string {
 }
 
 console.log(solution("ItisTimeToStudy"));
+
+function lectureSolution(s: string): string {
+  let answer = "";
+  for (let x of s) {
+    if (x === x.toLowerCase()) answer += x.toUpperCase();
+    else answer += x;
+  }
+  return answer;
+}
+
+console.log(lectureSolution("ItisTimeToStudy"));
+
+// 아스키코드
+function lectureSolution2(s: string): string {
+  let answer = "";
+  for (let x of s) {
+    let num = x.charCodeAt(0);
+    if (num >= 97 && num <= 122) answer += String.fromCharCode(num - 32);
+    else answer += x;
+  }
+  return answer;
+}
+
+console.log(lectureSolution2("ItisTimeToStudy"));
