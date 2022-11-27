7f44d85788e167289b39989ba43784325973a76d
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-11-27 22:43:06 +0900

    FIX: (강의) 11. 대문자 찾기 - 아스키코드값 활용

--- src/section_1/11.ts
+++ src/section_1/11.ts
@@ -6,3 +6,27 @@ function solution(s) {
 }
 
 console.log(solution("KoreaTimeGood"));
+
+function lectureSolution(s) {
+  let answer = 0;
+  for (let x of s) {
+    if (x === x.toUpperCase()) answer++;
+  }
+  return answer;
+}
+
+console.log(lectureSolution("KoreaTimeGood"));
+
+function lectureSolution2(s) {
+  let answer = 0;
+  for (let x of s) {
+    let num = x.charCodeAt();
+    // String.cahrCodeAt(): 해당 string의 아스키코드값을 넘겨준다
+    // 대문자 아스키코드 넘버 범위: 65 ~ 90
+    // 소문자 아스키코드 넘버 범위: 97 ~ 122
+    if (num >= 65 && num <= 90) answer++;
+  }
+  return answer;
+}
+
+console.log(lectureSolution2("KoreaTimeGood"));
