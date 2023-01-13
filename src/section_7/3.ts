cabb70a42a315d250be71686f9f2b2a695aa707a
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2023-01-13 23:22:55 +0900

    ADD: (먼저 풀어보기, 강의) section_7.3. Special Sort(구글 인터뷰)

---
+++ src/section_7/3.ts
@@ -0,0 +1,26 @@
+function solution(arr: number[]) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] < 0 && arr[j - 1] > 0) {
+        [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(solution([1, 2, 3, -3, -2, 5, 6, -6])); // -3 -2 -6 1 2 3 5 6
+
+function lectureSolution(arr: number[]) {
+  let answer = arr;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > 0 && arr[j + 1] < 0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return answer;
+}
+
+console.log(lectureSolution([1, 2, 3, -3, -2, 5, 6, -6])); // -3 -2 -6 1 2 3 5 6
