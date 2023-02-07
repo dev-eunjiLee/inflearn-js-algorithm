f71e4d0ccb79195c87d02d81364581e39526ba45
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-02-08 00:15:37 +0900

    ADD: (강의) section_8.3 이진트리 순회(깊이 우선 탐색) - 중위순회, 후위순회

--- src/section_8/3.ts
+++ src/section_8/3.ts
@@ -45,3 +45,23 @@ function lectureSolution2(v) {
 }
 
 console.log(lectureSolution2(1));
+
+// 후위순회
+function lectureSolution3(v) {
+  let answer;
+  function DFS(v) {
+    if (v > 7) {
+      // 종착지
+      return;
+    } else {
+      DFS(v * 2); // 왼쪽 자식 노드
+      DFS(v * 2 + 1); // 오른쪽 자식 노드
+      console.log(v);
+    }
+  }
+
+  DFS(v);
+  return answer;
+}
+
+console.log(lectureSolution3(1));
