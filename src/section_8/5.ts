a19beac5d3efd24f45d595ac041fab41934dd925
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-02-14 00:29:49 +0900

    ADD: section_8.5. 합이 같은 부분집합(DFS: 아마존 인터뷰) 끝

--- src/section_8/5.ts
+++ src/section_8/5.ts
@@ -1,14 +1,20 @@
 function solution(n: number[]) {
-  console.log(n);
+  let answer = "NO";
   const sum = n.reduce((acc, curr) => acc + curr, 0);
-  console.log(sum);
   const perSum = sum / 2;
-  for (let [index, per] of n.entries()) {
+  for (let [index, _] of n.entries()) {
+    if (answer === "YES") {
+      break;
+    }
     let tempSum = 0;
     for (let i = index; i < n.length; i++) {
+      if (tempSum === perSum) {
+        answer = "YES";
+        break;
+      }
       tempSum += n[i];
-      if (tempSum === perSum) return "YES";
-      else if (tempSum > perSum) {
+
+      if (tempSum > perSum) {
         let nowIndex = i;
         while (tempSum > perSum) {
           nowIndex -= 1;
@@ -17,6 +23,36 @@ function solution(n: number[]) {
       }
     }
   }
+  return answer;
 }
 
-console.log(solution([1, 3, 5, 6, 7, 10]));
+console.log(solution([1, 3, 5, 6, 7, 10])); // YES
+console.log(solution([1, 3, 5, 6, 9, 10])); // YES
+console.log(solution([4, 5, 100, 113])); // NO
+
+console.log("==============");
+
+function lectureSolution(arr: number[]) {
+  let answer = "NO";
+  let flag = 0;
+  let total = arr.reduce((acc, curr) => acc + curr, 0);
+  let length = arr.length;
+  function DFS(L, sum) {
+    if (flag) return; // 부분집합이 발견되고 난 이후의 함수는 길게 돌지 않도록
+    if (L === length) {
+      if (total - sum === sum) {
+        answer = "YES";
+        flag = 1;
+      }
+    } else {
+      DFS(L + 1, sum + arr[L]); // 해당 index의 값을 부분 집합에서 사용하는 경우
+      DFS(L + 1, sum); // 해당 index의 값을 부분 집합에서 사용하지 않는 경우
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
+
+console.log(lectureSolution([1, 3, 5, 6, 7, 10])); // YES
+console.log(lectureSolution([1, 3, 5, 6, 9, 10])); // YES
+console.log(lectureSolution([4, 5, 100, 113])); // NO
