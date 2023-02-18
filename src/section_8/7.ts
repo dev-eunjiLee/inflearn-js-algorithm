6cf61fdecc6b1705107b06c06721e285eb9bd319
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-02-19 00:17:40 +0900

    ADD: section_8.7 최대점수 구하기(DFS) 문제풀기 및 강의

--- src/section_8/7.ts
+++ src/section_8/7.ts
@@ -1,8 +1,38 @@
+type score = number;
+type time = number;
+
 function solution(
   limitTime: number,
-  scoreAndTime: Array<Array<number>>
+  scoreAndTime: Array<[score, time]>
 ): number {
-  return null;
+  let max = 0;
+  function DFS(
+    level: number,
+    totalScore: number,
+    totalTime: number,
+    list: Array<[score, time]>
+  ): number {
+    console.log(level, totalScore, totalTime, list);
+    if (totalTime > limitTime) {
+      return;
+    } else {
+      if (level === scoreAndTime.length) {
+        max = Math.max(totalScore, max);
+      } else {
+        DFS(
+          level + 1,
+          totalScore + scoreAndTime[level][0],
+          totalTime + scoreAndTime[level][1],
+          [...list, scoreAndTime[level]]
+        );
+        DFS(level + 1, totalScore, totalTime, list);
+      }
+    }
+  }
+
+  DFS(0, 0, 0, []);
+
+  return max;
 }
 
 console.log(
@@ -14,3 +44,27 @@ console.log(
     [7, 4],
   ])
 );
+
+function lectureSolution(
+  m: number,
+  ps: Array<number>,
+  pt: Array<number>
+): number {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let n = ps.length;
+  function DFS(l, sum, time) {
+    if (time > m) return;
+    if (l === n) {
+      answer = Math.max(answer, sum);
+    } else {
+      DFS(l + 1, sum + ps[l], time + pt[l]);
+      DFS(l + 1, sum, time);
+    }
+  }
+
+  DFS(0, 0, 0);
+
+  return answer;
+}
+
+console.log(lectureSolution(20, [10, 25, 15, 6, 7], [5, 12, 8, 3, 4]));
