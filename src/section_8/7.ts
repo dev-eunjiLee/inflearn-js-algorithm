ef4d71be9cd43812b0f3728e793c4a80a58dd17c
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-02-18 23:42:15 +0900

    ADD: section_8.7 최대점수 구하기(DFS) 문제 셋팅

---
+++ src/section_8/7.ts
@@ -0,0 +1,16 @@
+function solution(
+  limitTime: number,
+  scoreAndTime: Array<Array<number>>
+): number {
+  return null;
+}
+
+console.log(
+  solution(20, [
+    [10, 5],
+    [25, 12],
+    [15, 8],
+    [6, 3],
+    [7, 4],
+  ])
+);
