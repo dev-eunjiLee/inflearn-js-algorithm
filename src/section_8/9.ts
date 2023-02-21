d3b89fe65e9a35f6c03a29d4791d3befb621f610
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-02-21 23:37:20 +0900

    ADD: section_8.9 동전교환(DFS-Cut Edge Tech) 강의

--- src/section_8/9.ts
+++ src/section_8/9.ts
@@ -20,3 +20,28 @@ function solution(coinList: Array<number>, charges: number): coinCnt {
   return answer;
 }
 console.log(solution([1, 2, 5], 15));
+
+function lectureSolution(m, arr) {
+  let answer = Number.MAX_SAFE_INTEGER;
+  let n = arr.length;
+  function DFS(L, sum) {
+    if (sum > m) {
+      return;
+    }
+    if (answer <= L) {
+      return;
+    }
+    if (sum === m) {
+      console.log(L, sum);
+      answer = Math.min(answer, L);
+    } else {
+      for (let i = 0; i < n; i++) {
+        DFS(L + 1, sum + arr[i]);
+      }
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
+
+console.log(lectureSolution(15, [1, 2, 5]));
