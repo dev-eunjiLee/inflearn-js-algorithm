cbc624a3b0f0f185b58cafddbec72fabae7be64d
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-03-03 23:12:15 +0900

    [section_8.14] 조합구하기(수업)

--- src/section_8/14.ts
+++ src/section_8/14.ts
@@ -24,3 +24,22 @@ function solution(n: number, m: number): Array<Array<number> | number> {
 }
 
 console.log(solution(4, 2));
+
+function lectureSolution(n, m) {
+  let answer = [];
+  let tmp = Array.from({ length: m }, () => 0);
+  function DFS(L, s) {
+    if (L === m) {
+      answer.push(tmp.slice());
+      return;
+    } else {
+      for (let i = s; i <= n; i++) {
+        tmp[L] = i;
+        DFS(L + 1, i + 1);
+      }
+    }
+  }
+  DFS(0, 1);
+  return answer;
+}
+console.log(lectureSolution(4, 2));
