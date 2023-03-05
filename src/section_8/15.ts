0cef0dcbb9f27a522987a0b33e06b86bbce8ffd2
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-03-05 20:50:06 +0900

    [section_8.15] 수들의 조합(수업)

--- src/section_8/15.ts
+++ src/section_8/15.ts
@@ -18,8 +18,6 @@ function solution(
       return;
     } else {
       for (let i = startIndex; i < numberList.length; i++) {
-        if (tmpList[0] === 2 && tmpList[1] === 8) {
-        }
         if (checkArray[i] === false) {
           checkArray[i] = true;
           tmpList.push(numberList[i]);
@@ -36,3 +34,25 @@ function solution(
 }
 
 console.log(solution(3, [2, 4, 5, 8, 12], 6));
+
+function lectureSolution(k: number, arr: Array<number>, m: number): number {
+  let answer = 0;
+  let tmp = Array.from({ length: k });
+  function DFS(L, s, sum) {
+    if (L === k) {
+      console.log(sum, tmp);
+      if (sum % m === 0) {
+        answer++;
+      }
+    } else {
+      for (let i = s; i < arr.length; i++) {
+        tmp[L] = arr[i];
+        DFS(L + 1, i + 1, sum + arr[i]);
+      }
+    }
+  }
+  DFS(0, 0, 0);
+  return answer;
+}
+
+console.log(lectureSolution(3, [2, 4, 5, 8, 12], 6));
