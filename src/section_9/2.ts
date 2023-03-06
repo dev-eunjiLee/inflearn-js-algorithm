e9f1aaaa26998a32aaf3728d7d5a86cd050293b1
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-03-06 23:05:33 +0900

    [section_9.2] 경로탐색(인접행력)(수업)

--- src/section_9/2.ts
+++ src/section_9/2.ts
@@ -37,9 +37,61 @@ function solution(nodeGraphList: Array<Array<number>>) {
   // TODO 13만 나와서 확인 필요
   return answer;
 }
+// console.log(
+//   solution([
+//     [5, 9],
+//     [1, 2],
+//     [1, 3],
+//     [1, 4],
+//     [2, 1],
+//     [2, 3],
+//     [2, 5],
+//     [3, 4],
+//     [4, 2],
+//     [4, 5],
+//   ])
+// );
+
+function lectureSolution(n, arr) {
+  console.log(n, arr);
+
+  let answer = 0;
+  let graph = Array.from(Array(n + 1), () => Array(n + 1).fill(0)); // 1번 인덱스부터 사용
+  let ch = Array.from({ length: n + 1 }, () => 0);
+
+  for (let [a, b] of arr) {
+    console.log(a, b);
+    graph[a][b] = 1;
+  }
+
+  let path = [];
+
+  function DFS(v) {
+    if (v === n) {
+      answer++;
+      console.log(path);
+    } else {
+      for (let i = 1; i <= n; i++) {
+        // 모든 노드를 비교해서, 연결되어 있으면서 방문하지 않은 노드의 경우만 ch에 추가
+        if (graph[v][i] === 1 && ch[i] === 0) {
+          ch[i] = 1;
+          path.push(i);
+          DFS(i);
+          path.pop();
+          ch[i] = 0;
+        }
+      }
+    }
+  }
+  ch[1] = 1;
+  path.push(1);
+  DFS(1);
+
+  return answer;
+}
+
 console.log(
-  solution([
-    [5, 9],
+  lectureSolution(5, [
     [1, 2],
     [1, 3],
     [1, 4],
