c48529036a41ff91845336269dcade3d2f06fcbe
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-03-07 23:34:28 +0900

    [section_9.3] 경로탐색(인접리스트)(예습 - 문제셋팅)

---
+++ src/section_9/3.ts
@@ -0,0 +1,17 @@
+function solution(arr: Array<Array<number>>) {
+  console.log(arr);
+}
+
+console.log(
+  solution([
+    [1, 2],
+    [1, 3],
+    [1, 4],
+    [2, 1],
+    [2, 3],
+    [2, 5],
+    [3, 4],
+    [4, 2],
+    [4, 5],
+  ])
+);
