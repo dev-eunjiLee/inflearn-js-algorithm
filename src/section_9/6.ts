a8852fc6cf9751fd482fb3579e1c4de68c75c518
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-03-26 19:36:01 +0900

    [section_9.6] 송아지 찾기(BFS: 상대트리탐색) (강의)

--- src/section_9/6.ts
+++ src/section_9/6.ts
@@ -23,3 +23,32 @@ function solution(personIndex: number, cowIndex: number): number {
 }
 
 console.log(solution(5, 14)); // 3
+
+function lectureSolution(s, e) {
+  let answer = 0;
+  let ch = Array.from({ length: 10001 }, () => 0);
+  let dis = Array.from({ length: 10001 }, () => 0);
+
+  let queue = [];
+
+  ch[s] = 1;
+  queue.push(s);
+  dis[s] = 0;
+
+  while (queue.length !== 0) {
+    let x = queue.shift();
+    for (let nx of [x - 1, x + 1, x + 5]) {
+      if (nx === e) return dis[x] + 1;
+      if (nx > 0 && nx <= 10000 && ch[nx] === 0) {
+        ch[nx] = 1;
+        queue.push(nx);
+        dis[nx] = dis[x] + 1;
+      }
+    }
+  }
+
+  console.log(3);
+}
+
+console.log(lectureSolution(5, 14));
+console.log(lectureSolution(8, 3));
