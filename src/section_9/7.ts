903307106bd654d28c8810c2112f02c2b4bde365
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-03-27 15:30:52 +0900

    [section_9.7] 섬나라 아일랜드(BFS)활용(수업 듣는중 9:56)

--- src/section_9/7.ts
+++ src/section_9/7.ts
@@ -71,8 +71,93 @@ function solution(arr: Array<Array<number>>): number {
   return answer;
 }
 
+// console.log(
+//   solution([
+//     [1, 1, 0, 0, 0, 1, 0],
+//     [0, 1, 1, 0, 1, 1, 0],
+//     [0, 1, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 1, 0, 1, 1],
+//     [1, 1, 0, 1, 1, 0, 0],
+//     [1, 0, 0, 0, 1, 0, 0],
+//     [1, 0, 1, 0, 1, 0, 0],
+//   ])
+// );
+
+function lectureSolution(board) {
+  let answer = 0;
+  let n = board.length;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  // let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  // let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  let queue = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        board[i][j] = 0;
+        queue.push([i, j]);
+        answer++;
+        while (queue.length) {
+          let [x, y] = queue.shift();
+          for (let k = 0; i < 8; k++) {
+            let nx = x + dx[k];
+            let ny = y + dy[k];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+              board[nx][ny] = 0;
+              queue.push([nx, ny]);
+            }
+          }
+        }
+      }
+    }
+  }
+  return answer;
+}
+// console.log(
+//   lectureSolution([
+//     [1, 1, 0, 0, 0, 1, 0],
+//     [0, 1, 1, 0, 1, 1, 0],
+//     [0, 1, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 1, 0, 1, 1],
+//     [1, 1, 0, 1, 1, 0, 0],
+//     [1, 0, 0, 0, 1, 0, 0],
+//     [1, 0, 1, 0, 1, 0, 0],
+//   ])
+// );
+
+function lectureCopySolution(board) {
+  let answer = 0;
+  let n = board.length;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+  let queue = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        board[i][j] = 0;
+        queue.push([i, j]);
+        answer++;
+        while (queue.length) {
+          let x = queue.shift();
+          for (let k = 0; k < 8; k++) {
+            let nx = x[0] + dx[k];
+            let ny = x[1] + dy[k];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+              board[nx][ny] = 0;
+              queue.push([nx, ny]);
+            }
+          }
+        }
+      }
+    }
+  }
+  return answer;
+}
+
 console.log(
-  solution([
+  lectureCopySolution([
     [1, 1, 0, 0, 0, 1, 0],
     [0, 1, 1, 0, 1, 1, 0],
     [0, 1, 0, 0, 0, 0, 0],
