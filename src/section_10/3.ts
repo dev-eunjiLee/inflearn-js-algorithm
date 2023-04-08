93eb1f4397bacf14b2d441c3be00e63eb1a20138
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-04-09 01:41:44 +0900

    [section_10.3] 최대부분증가수열(LIS) 예습2

--- src/section_10/3.ts
+++ src/section_10/3.ts
@@ -4,22 +4,16 @@ function solution(arr: Array<number>): number {
 
   dy[0] = 1;
 
-  // 1. 나보다 앞 index의 값들을 확인
   for (let i = 1; i < arr.length; i++) {
-    const tmp = [];
-    for (let j = 0; j < i; j++) {
-      // 2. 나의 앞 값이 나보다 작은 경우, 증가수열에 넣을 수 있음 => 앞의 값까지만 포함된 증가수열의 길이를 tmp에 push
-      if (arr[i] > arr[j]) {
-        tmp.push(dy[j]);
-      }
-    }
-    console.log(`tmp: ${tmp}`);
-    // 3. push된 tmp 중에서 가장 긴 값에 + 1 해서 증가수열 길이 처리
-    if (tmp.length === 0) {
-      dy[i] = 1;
-    } else {
-      dy[i] = Math.max(...tmp) + 1;
-    }
+    // index가 기준보다 작고, value도 작은 경우, 증가수열에 포함 가능
+    const max = Math.max(
+      ...arr.map((value, index) => {
+        if (index < i && value < arr[i]) return dy[index];
+        else return null;
+      })
+    );
+    if (max === 0) dy[i] = 1;
+    else dy[i] = max + 1;
   }
 
   return Math.max(...dy);
