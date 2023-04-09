801b487777711e5497e3d0f318e734bd3bcf57c1
dev-eunjiLee <dev.eunji.lee@gmail.com>
2023-04-09 17:47:50 +0900

    [section_10.4] 동전교환(냅색 알고리즘)강의

--- src/section_10/4.ts
+++ src/section_10/4.ts
@@ -20,5 +20,30 @@ function solution(charges: number, coinList: Array<number>): number {
   return coinCnt;
 }
 
+console.time("1");
 console.log(`answer: ${solution(15, [1, 2, 5])}`); // 3
 console.log(`answer: ${solution(17, [1, 2, 5])}`); // 4
+console.timeEnd("1");
+
+function lectureSolution(m, coin) {
+  let answer = 0;
+  const MAX_SAFE_NUMBER = 1000;
+  let dy = Array.from({ length: m + 1 }, () => MAX_SAFE_NUMBER);
+
+  dy[0] = 0;
+
+  for (let i = 0; i < coin.length; i++) {
+    // 동전 금액부터 ++해서 진행하도록
+    for (let j = coin[i]; j < m + 1; j++) {
+      // 기존 값과, 해당 코인을 뺀 값의 최소 금액 + 1 과 비교해서 더 작은 값으로 업데이트
+      dy[j] = Math.min(dy[j], dy[j - coin[i]] + 1);
+    }
+  }
+
+  return dy[m];
+}
+
+console.time("2");
+console.log(`answer: ${lectureSolution(15, [1, 2, 5])}`); // 3
+console.log(`answer: ${lectureSolution(17, [1, 2, 5])}`); // 4
+console.timeEnd("2");
